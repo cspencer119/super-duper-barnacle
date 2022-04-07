522244feca2cb52bd4eee423f2519c22f9199b37
Cyrus Spencer <cyrus.spencer119@gmail.com>
2022-04-07 13:42:44 -0400

    Cyrus/7/ItemController First Commit

--- FinalProject/Controllers/ItemController.cs
+++ FinalProject/Controllers/ItemController.cs
@@ -9,5 +9,45 @@ namespace FinalProject.Controllers
 {
     public class ItemController : ApiController
     {
+        public IHttpActionResult Get()
+        {
+            var cService = CreateItemService();
+            var items = cService.GetItems();
+            return Ok(items);
+        }
+
+        public IHttpActionResult Post(ItemCreate item)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var cService = CreateItemService();
+            if (!cService.CreateItem(item))
+            {
+                return InternalServerError();
+            }
+            return Ok();
+        }
+
+        public IHttpActionResult Edit(items)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var cService = CreateItemService();
+            if (!cService.EditItem(items))
+            {
+                return InternalServerError();
+            }
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateItemService();
+
+            if (!service.DeleteItems(id))
+                return InternalServerError();
+
+            return Ok();
+        }
     }
 }
