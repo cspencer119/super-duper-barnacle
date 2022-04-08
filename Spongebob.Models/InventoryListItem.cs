8cb5b9c852ef75f28a58693bda39526dacc9ef07
Hayden Linville <hayden.linville@gmail.com>
2022-04-08 11:20:46 -0400

    inventory models added

---
+++ Spongebob.Models/InventoryListItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spongebob.Models
+{
+    public class InventoryListItem
+    {
+        public int InventoryId { get; set; }
+        public int ItemId { get; set; }
+        public int CharacterId { get; set; }
+
+    }
+}
