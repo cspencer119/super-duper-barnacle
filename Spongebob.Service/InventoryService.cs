859583e47085ce23c2d55515e4f66f2dfd11daab
Cyrus Spencer <cyrus.spencer119@gmail.com>
2022-04-08 13:14:20 -0400

    Cyrus/32/InventoryService Commit

---
+++ Spongebob.Service/InventoryService.cs
@@ -0,0 +1,110 @@
+﻿using Spongebob.Data;
+using Spongebob.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spongebob.Service
+{
+    public class InventoryService
+    {
+        
+        
+            private readonly Guid _userId;
+
+            public InventoryService(Guid userId)
+            {
+                _userId = userId;
+            }
+            public bool CreateInventory(InventoryCreate model)
+            {
+                var entity =
+                    new Inventory()
+                    {
+                        CharacterId = model.CharacterId,
+                        ItemId = model.ItemId,
+                       
+                    };
+
+                using (var ctx = new ApplicationDbContext())
+                {
+                    ctx.Inventory.Add(entity);
+                    return ctx.SaveChanges() == 1;
+                }
+            }
+
+            public IEnumerable<InventoryListItem> GetInventory()
+            {
+                using (var ctx = new ApplicationDbContext())
+                {
+                    var query =
+                        ctx
+                        .Inventories
+                        .Where(e => e.UserId == _userId)
+                        .Select(
+                            e =>
+                            new InventoryListItem
+                            {
+                                ItemId = e.ItemId,
+                                InventoryId = e.InventoryId,
+                                CharacterId = e.CharacterId,
+                            }
+                            );
+                    return query.ToArray();
+                }
+            }
+
+            public InventoryDetail GetInventoryById(int id)
+            {
+                using (var ctx = new ApplicationDbContext())
+                {
+                    var entity =
+                        ctx
+                        .Inventories
+                        .Single(e => e.InventoryId == id && e.UserId == _userId);
+                    return
+                        new InventoryDetail
+                        {
+                            ItemId = entity.ItemId,
+                            InventoryId = entity.InventoryId,
+                            CharacterId = entity.CharacterId,
+                        };
+                }
+            }
+
+            public bool EditInventory(InventoryEdit model)
+            {
+                using (var ctx = new ApplicationDbContext())
+                {
+                    var entity =
+                        ctx
+                        .Inventories
+                        .Single(e => e.InventoryId == model.InventoryId && e.UserId == _userId);
+
+                    entity.ItemId = model.ItemId;
+                    entity.InventoryId = model.InventoryId;
+                entity.CharacterId = model.CharacterId;
+                    return ctx.SaveChanges() == 1;
+                }
+            }
+
+            public bool DeleteInventory(int inventoryId)
+            {
+                using (var ctx = new ApplicationDbContext())
+                {
+                    var entity =
+                        ctx
+                        .Inventories
+                        .Single(e => e.InventoryId == inventoryId && e.UserId == _userId);
+
+                    ctx.Inventories.Remove(entity);
+
+                    return ctx.SaveChanges() == 1;
+                }
+            }
+
+        }
+    }
+}
