f75636c68fe02d0071c568f8eeb9200b15828766
Hayden Linville <hayden.linville@gmail.com>
2022-04-11 14:40:23 -0400

    added item character detail model and added way to see inventory details to getcharacterbyid

---
+++ Spongebob.Models/Item/ItemCharacterDetail.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spongebob.Models.Item
+{
+    public class ItemCharacterDetail
+    {
+        public string ItemName { get; set; }
+    }
+}
