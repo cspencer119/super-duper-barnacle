10afcbb655fa94f818e67df1dc1104829ee8262d
Ethan Starling <ethan.jr.starling@gmail.com>
2022-04-15 10:58:00 -0400

    added in return null method to get char by id

--- FinalProject/Controllers/InventoryController.cs
+++ FinalProject/Controllers/InventoryController.cs
@@ -9,28 +9,24 @@ using System.Net.Http;
 using System.Web.Http;
 
 namespace FinalProject.Controllers
-{
-    
+{  
     public class InventoryController : ApiController
     {
-
         public IHttpActionResult Get()
         {
             InventoryService iService = CreateInventoryService();
             var inventory = iService.GetInventory();
             return Ok(inventory);
         }
+
         [Authorize]
         public IHttpActionResult Post(InventoryCreate model)
         {
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
-
             var service = CreateInventoryServiceUserId();
-
             if (!service.CreateInventory(model))
                 return InternalServerError();
-
             return Ok();
         }
 
@@ -45,28 +41,20 @@ namespace FinalProject.Controllers
         {
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
-
             var iService = CreateInventoryService();
-
             if (!iService.UpdateInventory(inventory))
                 return BadRequest("The InventoryId you provided does not exist");
-
             return Ok();
-
         }
+
         [Authorize]
         public IHttpActionResult Delete(int id)
         {
             var iService = CreateInventoryServiceUserId();
-
             if (!iService.DeleteInventory(id))
                 return BadRequest("You can only delete Inventories that you have created. This Inventory either does not exist or was not created by you!");
-
             return Ok();
-
         }
-
-
 
         private InventoryService CreateInventoryService()
         {
@@ -74,14 +62,11 @@ namespace FinalProject.Controllers
             return inventoryService;
         }
 
-
         private InventoryService CreateInventoryServiceUserId()
         {
             var userId = Guid.Parse(User.Identity.GetUserId());
             var inventoryService = new InventoryService(userId);
             return inventoryService;
         }
-
-
     }
 }
